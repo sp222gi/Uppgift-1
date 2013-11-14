7b1c4131f61aadfe420381699669ec63dfd6e496
sp222gi <sp222gi@student.lnu.se>
2013-11-14 13:59:12 +0100

    Testing Commits

---
+++ Tri/TriTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TriTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void isIsosceles()
+        {
+            Triangle tri = new Triangle(1.0, 0.5, 1.0);
+            Assert.IsTrue(tri.isIsosceles());
+        }
+    }
+}
